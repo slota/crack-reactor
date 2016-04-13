e7126a9546a2b5755e29881ad35d815c528ec1fc
John Slota <john.slota@gmail.com>
2016-04-13 17:56:05 -0600

    added header, all skills and skill to react app

---
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -0,0 +1,10 @@
+var AllSkills = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1> These are all my skills </h1>
+        <h2> super kewl story breh </h2>
+      </div>
+    )
+  }
+});
---
+++ app/assets/javascripts/components/_header.js.jsx
@@ -0,0 +1,9 @@
+var Header = React.createClass({
+  render() {
+    return (
+      <div>
+        <Body />
+      </div>
+    )
+  }
+});
