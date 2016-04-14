254422234d9a8777152e940ee86e179fa6cabce6
John Slota <john.slota@gmail.com>
2016-04-14 11:30:21 -0600

    delete finished

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -1,19 +1,24 @@
 var AllSkills = React.createClass({
 
+  handleDelete(id) {
+    this.props.handleDelete(id);
+  },
+
   render() {
     var skills = this.props.skills.map((skill) => {
       return (
         <div key={skill.id}>
-        <h3>{skill.name}</h3>
-        <p><strong>Level:</strong> {skill.level}</p>
-        <p>{skill.details}</p>
+          <h3>{skill.name}</h3>
+          <p><strong>Level:</strong> {skill.level}</p>
+          <p>{skill.details}</p>
+          <button onClick={this.handleDelete.bind(this, skill.id)}>Delete</button>
         </div>
       )
     });
-    
+
     return(
       <div>
-      {skills}
+        {skills}
       </div>
     )
   }
