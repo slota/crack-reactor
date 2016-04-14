cc6247c63f0c297b8bb4909730ebbee3659df7fe
John Slota <john.slota@gmail.com>
2016-04-14 11:08:28 -0600

    figured out the bug and things are working again

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -1,29 +1,21 @@
 var AllSkills = React.createClass({
-  getInitialState() {
-    return { skills: [] }
-  },
-
-  componentDidMount() {
-    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
-  },
 
   render() {
-    var skills = this.state.skills.map((skill) => {
+    var skills = this.props.skills.map((skill) => {
       return (
-        <div>
-          <h3>{skill.name}</h3>
-          <p><strong>Level:</strong> {skill.level}</p>
-          <p>{skill.details}</p>
+        <div key={skill.id}>
+        <h3>{skill.name}</h3>
+        <p><strong>Level:</strong> {skill.level}</p>
+        <p>{skill.details}</p>
         </div>
       )
     });
-
+    
     return(
       <div>
-        {skills}
+      {skills}
       </div>
     )
   }
-
 
 });
